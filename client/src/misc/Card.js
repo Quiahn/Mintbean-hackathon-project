4a17c0b2ccb0d820366d9a45df1be9668224f155
Sardorbek Omonkulov <somonkulov2018@gmail.com>
2021-07-30 20:14:45 -0400

    Card animations are smooth as a butter!

--- client/src/misc/Card.js
+++ client/src/misc/Card.js
@@ -1,30 +1,33 @@
-import React, { useState, useEffect } from 'react'
-import { useSpring, animated, config } from 'react-spring'
+import React from 'react'
+import { useSpring, animated, config, Interpolation } from 'react-spring'
+import backImg from '../icons/card-back-v2.jpg'
 
 
-
-export default function Card({ cardPos, isRed, cardFlip }) {
-
-    const [cardPositon, setCardPositon] = useState(cardPos)
-    const [red, setRed] = useState(isRed)
-    const [flip, setFlip] = useState(cardFlip)
-
+export default function Card({ cardPos, cardFlip, rotation, id, moveId, allMove }) {
     const props = useSpring({
-        to: { ...cardPositon, transform: `rotate(${getRandomInt(-20, 20)}deg) translate(-50%, -50%)`, },
+        to: (allMove || (moveId === id)) ? { ...cardPos, transformOrigin: "center, center", transform: `rotate(${rotation}deg) translate(-50%, -50%)`, } : null,
         config: config.stiff,
     })
 
-    useEffect(() => {
-        setCardPositon(cardPos)
-    }, [cardPos, isRed, cardFlip])
+    const { transform, opacity } = useSpring({
+        opacity: (moveId === id) ? cardFlip ? 1 : 0 : null,
+        transform: (moveId === id) ? `perspective(600px) rotateY(${cardFlip ? 180 : 0}deg)` : null,
+        config: { mass: 5, tension: 500, friction: 80 },
+    })
 
     return (
-        <animated.div style={props} className={`h-32 w-24 lg:h-52 lg:w-32 absolute origin-center ${(red) ? ` bg-red-300` : `bg-blue-300`}`} >
-            <br />
+        <animated.div style={props} className="game-card absolute rotation-center-card" >
+            <animated.img src={backImg} className="absolute" alt="Back"
+                style={{
+                    opacity,
+                    transform,
+                    rotateY: '180deg',
+                }}
+            />
+
+            <animated.img src={backImg} className="absolute" alt="Back"
+                style={(opacity) ? { opacity: opacity.to(o => 1 - o), transform } : null}
+            />
         </animated.div>
     )
-}
-
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
 }
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Card from '../misc/Card';
 
 
@@ -8,7 +8,7 @@ const bluePositions = {
     at_win_stack: { top: `100%`, left: `15%` },
     at_loss_stack: { top: `0%`, left: `70%` },
     at_shuffle: { top: `50%`, left: `10%` },
-
+    at_middle: { top: `50%`, left: `50%` }
 }
 
 
@@ -19,24 +19,100 @@ const redPositions = {
     at_win_stack: { top: `0%`, left: `70%` },
     at_loss_stack: { top: `100%`, left: `15%` },
     at_shuffle: { top: `50%`, left: `10%` },
+    at_middle: { top: `50%`, left: `50%` }
 }
 
+const redDeck = (redP, moveId, allMove, flipCard) => {
+    let key = 0;
+    return Array.apply(null, { length: 26 }).map((e, i) => (
+        <Card key={key++} id={key} cardPos={redP} isRed={false} moveId={moveId} allMove={allMove} cardFlip={flipCard} rotation={getRandomInt(-20, 20)}></Card>
+    ));
+}
 
+const blueDeck = (blueP, moveId, allMove, flipCard) => {
+    let key = 0;
+    return Array.apply(null, { length: 26 }).map((e, i) => (
+        <Card key={key++} id={key} cardPos={blueP} isRed={true} moveId={moveId} allMove={allMove} cardFlip={flipCard} rotation={getRandomInt(-20, 20)}></Card>
+    ));
+}
 
 export default function About() {
-    const [redP, setRedP] = useState(redPositions.at_hand)
-    const [blueP, setBlueP] = useState(bluePositions.at_hand)
+    const [redP, setRedP] = useState(redPositions.at_middle)
+    const [blueP, setBlueP] = useState(bluePositions.at_middle)
+    const [flip, setFlip] = useState(false)
+    const [moveId, setMoveId] = useState(0)
+    const [allMove, setAllMove] = useState(true)
+    const [playPool, setPlayPool] = useState([])
+
+    useEffect(() => {
+        if (moveId > 26) {
+            setAllMove(true)
+            setMoveId(0)
+            setRedP(redPositions.at_middle)
+            setBlueP(bluePositions.at_middle)
+        }
+        setAllMove(false)
+    }, [moveId])
+
+    function atHand() {
+        setMoveId(0);
+        setAllMove(true);
+        setRedP(redPositions.at_hand);
+        setBlueP(bluePositions.at_hand)
+    }
+
+    function atShuffle(params) {
+        setMoveId(0);
+        setAllMove(true);
+        setRedP(redPositions.at_shuffle);
+        setBlueP(bluePositions.at_shuffle)
+    }
+
+    function atMiddle(params) {
+        setMoveId(0);
+        setAllMove(true);
+        setRedP(redPositions.at_middle);
+        setBlueP(bluePositions.at_middle);
+    }
+
+    function atPlay(params) {
+        setMoveId((moveId) => moveId + 1);
+        setAllMove(false);
+        setRedP(redPositions.at_play);
+        setBlueP(bluePositions.at_play)
+    }
+    function atBlueWin(params) {
+        setAllMove(false);
+        setRedP(redPositions.at_loss_stack);
+        setBlueP(bluePositions.at_win_stack);
+    }
+    function atRenWin(params) {
+        setAllMove(false);
+        setRedP(redPositions.at_win_stack);
+        setBlueP(bluePositions.at_loss_stack);
+    }
+
+
+
+
+
     return (
         <div className={"h-full flex flex-col overflow-hidden "}>
             <div className="h-5/6 w-full bg-indigo-50  overflow-hidden relative">
-                <Card cardPos={redP} isRed={true}></Card>
-                <Card cardPos={blueP} isRed={false}></Card>
+                {redDeck(redP, moveId, allMove, flip)}
+                {blueDeck(blueP, moveId, allMove, flip)}
             </div>
-            <button onClick={() => setRedP(redPositions.at_hand)}>At hand</button>
-            <button onClick={() => setRedP(redPositions.at_play)}>At Play</button>
-            <button onClick={() => setRedP(redPositions.at_loss_stack)}>At Loss</button>
-            <button onClick={() => setRedP(redPositions.at_win_stack)}>At Win</button>
-            <button onClick={() => setRedP(redPositions.at_shuffle)}>At Shuffle</button>
+            <button onClick={() => atHand()}>atHand</button>
+            <button onClick={() => atShuffle()}>atShuffle</button>
+            <button onClick={() => atMiddle()}>atMiddle</button>
+            <button onClick={() => atPlay()}>atPlay</button>
+            <button onClick={() => atBlueWin()}>atBlueWin</button>
+            <button onClick={() => atRenWin()}>atRedWin</button>
         </div>
     )
+}
+
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
 }
