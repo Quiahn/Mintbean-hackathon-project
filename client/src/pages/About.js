229754e778d13334fdd47d3d65ea51a892594b37
Sardorbek Omonkulov <somonkulov2018@gmail.com>
2021-08-01 15:10:41 -0400

    PUSHING SO SYNC LANDING PAGE

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -14,12 +14,64 @@ function genRandomArr2() {
 const imageArr = genRandomArr1().concat(genRandomArr2())
 
 export default function About() {
-
+    const [zIndexArr, setZindex] = useState(imageArr)
+    const [drawnRedCardsIndex, setDrawnRedCardsIndex] = useState([])
+    const [drawnBlueCardsIndex, setDrawnBlueCardsIndex] = useState([])
     const [positions, setPositions] = useState([
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
     ])
-    const [zIndexArr, setZindex] = useState(imageArr)
+    const [play, setPlay] = useState(false)
+
+
+    useEffect(() => {
+        if (play) {
+            redDraw()
+            blueDraw()
+            setPlay(false)
+        }
+    }, [play, redDraw, blueDraw])
+
+    const redDraw = () => {
+        drawACard(true, 0, 25, 2)
+    }
+
+    const blueDraw = () => {
+        drawACard(false, 26, 51, 5)
+    }
+
+    const blueDrawWar = () => {
+
+    }
+
+    const redDrawWar = () => {
+
+    }
+
+
+
+    const drawACard = (isRed, startIndex, endIndex, pos) => {
+        if (isRed) {
+            if (drawnRedCardsIndex.length > 25) return
+            let randomCard = getRandomIntInclusive(startIndex, endIndex)
+            console.log(drawnRedCardsIndex.length);
+            //If random card was already drawn, then try again until
+            while (drawnRedCardsIndex.includes(randomCard)) {
+                randomCard = getRandomIntInclusive(startIndex, endIndex)
+            }
+            setDrawnRedCardsIndex(prevArr => [...prevArr, randomCard])
+            changeArrayPos(randomCard, pos)
+        } else {
+            if (drawnBlueCardsIndex.length > 25) return
+            let randomCard = getRandomIntInclusive(startIndex, endIndex)
+            //If random card was already drawn, then try again until
+            while (drawnBlueCardsIndex.includes(randomCard)) {
+                randomCard = getRandomIntInclusive(startIndex, endIndex)
+            }
+            setDrawnBlueCardsIndex(prevArr => [...prevArr, randomCard])
+            changeArrayPos(randomCard, pos)
+        }
+    }
 
     const changeArrayPos = (index, pos) => {
         let newArr = positions
@@ -35,10 +87,11 @@ export default function About() {
                 ))}
             </div>
             <div className="h-1/6 bg-red-50">
-                <button onClick={() => changeArrayPos(getRandomIntInclusive(26, 51), 5)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >Blue Play</button>
-                <button onClick={() => changeArrayPos(getRandomIntInclusive(0, 25), 2)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >Red Play</button>
-                <button onClick={() => changeArrayPos(getRandomIntInclusive(26, 51), 7)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >Blue War Stack</button>
-                <button onClick={() => changeArrayPos(getRandomIntInclusive(0, 25), 7)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >Red War Stack</button>
+                <button onClick={() => blueDraw()} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >Blue Play</button>
+                <button onClick={() => redDraw()} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >Red Play</button>
+                <button onClick={() => blueDrawWar()} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >Blue War Stack</button>
+                <button onClick={() => redDrawWar()} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >Red War Stack</button>
+                <button onClick={() => setPlay(true)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >FLIP</button>
             </div>
         </div>
     )
